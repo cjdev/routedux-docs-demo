277e3c796dcb09bc764b2784e4f4aaa84993cc73
Max Summe <msumme@cj.com>
2017-06-19 10:27:34 -0700

    Removing defunct tests for routeaware page

--- src/components/Page.test.js
+++ src/components/Page.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ezJson from 'enzyme-to-json';
 import {shallow} from 'enzyme';
-import {Page, RouteAwarePageBuilder, mapStateToProps} from './Page';
-import {actions} from '../model';
+import {Page, mapStateToProps} from './Page';
 
 it("should render menu and contents", () => {
   //given
@@ -13,73 +12,6 @@ it("should render menu and contents", () => {
   //then
   expect(ezJson(wrapper)).toMatchSnapshot();
 
-});
-
-
-it("RouteAwarePage should load content using id from route and fire route transition action", () => {
-  //given
-  let idUsed;
-  const stubGetContents = (id) => {
-    idUsed = id;
-    return "Some content";
-  };
-  const stubStore = {
-    actions: [],
-    dispatch: function(action) {this.actions.push(action) }
-  };
-  const RouteAwarePage = RouteAwarePageBuilder(stubGetContents, stubStore);
-  const match = {params: {id: 'routeId'}};
-
-  //when
-  const wrapper = shallow(<RouteAwarePage match={match} menu="Menu" title="Title" />);
-
-  //then
-  expect(stubStore.actions[0]).toEqual(actions.changeId('routeId'));
-  expect(idUsed).toEqual('routeId');
-  expect(ezJson(wrapper)).toMatchSnapshot();
-
-});
-it("RouteAwarePage should load content using passed id instead when it is present", () => {
-//given
-  let idUsed;
-  const stubGetContents = (id) => {
-    idUsed = id;
-    return "Some content";
-  };
-  const stubStore = {
-    actions: [],
-    dispatch: function(action) {this.actions.push(action) }
-  };
-  const RouteAwarePage = RouteAwarePageBuilder(stubGetContents, stubStore);
-  const match = {params: {id: 'routeId'}};
-
-  //when
-  const wrapper = shallow(<RouteAwarePage id="ownId" match={match} menu="Menu" title="Title" />);
-
-  //then
-  expect(stubStore.actions[0]).toEqual(actions.changeId('ownId'));
-  expect(idUsed).toEqual('ownId');
-  expect(ezJson(wrapper)).toMatchSnapshot();
-});
-
-it("RouteAwarePage uses passed contents by default when content cannot be loaded", () => {
-  let idUsed;
-  const stubGetContents = (id) => {
-    idUsed = id;
-    return null;
-  };
-  const stubStore = {
-    actions: [],
-    dispatch: function(action) {this.actions.push(action) }
-  };
-  const RouteAwarePage = RouteAwarePageBuilder(stubGetContents, stubStore);
-  const match = {params: {id: 'routeId'}};
-
-  //when
-  const wrapper = shallow(<RouteAwarePage contents="Hello" match={match} menu="Menu" title="Title" />);
-
-  //then
-  expect(ezJson(wrapper)).toMatchSnapshot();
 });
 
 it("mapStateToProps uses its own title and contents preferentially", () => {
