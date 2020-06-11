598718e618c3a76228e2d97ef6f6d6a6970dda06
Ed Langley <el-github@elangley.org>
2020-06-11 10:16:43 -0700

    chore: upgrade routedux dep, reformat

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './App.css';
-import '@cjdev/visual-stack/lib/global';
-import {VIEW_ARTICLE} from './model/actions'
-import { SideNav } from '@cjdev/visual-stack/lib/components/SideNav';
-import {ActionLink} from 'routedux';
-import { Panel, Body, Header } from '@cjdev/visual-stack/lib/components/Panel';
-import ApplicationLayout from '@cjdev/visual-stack/lib/layouts/ApplicationLayout';
-import {connect} from 'react-redux';
-import {getContentsById} from './model';
+import React from "react";
+import "./App.css";
+import "@cjdev/visual-stack/lib/global";
+import { VIEW_ARTICLE } from "./model/actions";
+import { SideNav } from "@cjdev/visual-stack/lib/components/SideNav";
+import { ActionLink } from "routedux";
+import { Panel, Body, Header } from "@cjdev/visual-stack/lib/components/Panel";
+import ApplicationLayout from "@cjdev/visual-stack/lib/layouts/ApplicationLayout";
+import { connect } from "react-redux";
+import { getContentsById } from "./model";
 
-function App({title, contents, tableOfContents}) {
-
+function App({ title, contents, tableOfContents }) {
   const menu = (
     <SideNav onClick={() => {}}>
-      {tableOfContents.map((item) => {
+      {tableOfContents.map(item => {
         return (
-          <ActionLink key={item.id} action={{type: VIEW_ARTICLE, id: item.id}}>
+          <ActionLink
+            key={item.id}
+            action={{ type: VIEW_ARTICLE, id: item.id }}
+          >
             {item.title}
           </ActionLink>
         );
@@ -28,21 +30,25 @@ function App({title, contents, tableOfContents}) {
       <Panel>
         <Header>{title}</Header>
         <div className="article-body">
-          <Body><div dangerouslySetInnerHTML={{__html: contents}}/></Body>
+          <Body>
+            <div dangerouslySetInnerHTML={{ __html: contents }} />
+          </Body>
         </div>
       </Panel>
     </ApplicationLayout>
   );
 }
 
-const mapStateToProps = ({currentTitle:title, id, ...state}, {tableOfContents}) => {
-
+const mapStateToProps = (
+  { currentTitle: title, id, ...state },
+  { tableOfContents }
+) => {
   const foundContents = getContentsById(id, state);
-  const contents = foundContents ? foundContents : 'Loading...';
+  const contents = foundContents ? foundContents : "Loading...";
 
-  return {title, contents, tableOfContents};
+  return { title, contents, tableOfContents };
 };
 
 export default connect(mapStateToProps)(App);
 
-export {App, mapStateToProps};
+export { App, mapStateToProps };
